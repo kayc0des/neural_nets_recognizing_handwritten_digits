315e09322b4dc74d8b2a7a8c93a18faef4a16de1
kayc0des <k.boafo@alustudent.com>
2024-05-09 16:04:14 +0200

    network.py

--- network.py
+++ network.py
@@ -6,7 +6,17 @@ import numpy as np
 class Network(object):
     
     def __init__(self, sizes):
-        self.num_layers = len(sizes)
+        '''
+        Constructor method
+        
+        Param: sizes - List contains the number of neurons in the respective layers.
+        '''
+        self.num_layers = len(sizes) #the number of layers = to the length of the list
         self.sizes = sizes
-        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]] # biases are ignored for the input layer
         self.weights = [np.random.randn(y, x) for x,y in zip(sizes[:-1], sizes[1:])]
+        
+# debug
+net = Network([2, 3, 1])
+print(net.weights)
+print(net.biases)
