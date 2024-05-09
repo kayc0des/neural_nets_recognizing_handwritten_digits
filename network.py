c08a89cde4315eb8eebd8e7a0acfd6be57055049
kayc0des <k.boafo@alustudent.com>
2024-05-09 14:59:45 +0200

    network object

---
+++ network.py
@@ -0,0 +1,12 @@
+''' A network class: used to represent a neural network'''
+
+import numpy as np
+
+# network class
+class Network(object):
+    
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x,y in zip(sizes[:-1], sizes[1:])]
